af427d50175493cf99e1620f971b770718860deb
lexneconst <songkeart@gmail.com>
2023-09-30 18:00:19 +0700

    xdump v1.01

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ git config --global user.name "lexneconst"
 git add . 
 git add --all
 git status
+git branch -M main
 git commit -am 'xdump v1.01'
 git remote add origin 'https://github.com/lexneconst/xdump.git'
 git push origin 'main'
